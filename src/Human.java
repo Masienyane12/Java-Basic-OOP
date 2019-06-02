d4167caf2492b2c23c7dcba6e797211df383d411
Masienyane12 <prince.masienyane@gmail.com>
2019-06-02 09:51:36 +0200

    added Human Class, that will be used for Inheritance

---
+++ src/Human.java
@@ -0,0 +1,56 @@
+public class Human {
+
+    String name;
+
+    String surname;
+    int age;
+    String id;
+
+    public  Human(){
+
+    }
+
+
+    public Human(String name, String surname, int age, String id) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+
+
+}
